554ac172e4210d2b790543c79f614e7107121af7
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-10-01 13:37:45 +0300

    style: scss display classes function added

--- components/layouts/TheHeader.vue
+++ components/layouts/TheHeader.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
     <header>
-      <nav class="d-flex h-10">
-        <h3 class="m-0 p-2"><nuxt-link to="/"> Mob Fighter</nuxt-link></h3>
+      <nav class="d-flex h-10rem">
+        <h3 class="m-2 p-2"><nuxt-link to="/"> Mob Fighter</nuxt-link></h3>
         <ul class="d-flex m-0">
-          <li class="mx-1 p-3">
+          <li class="h-3 mx-1 p-2">
             <nuxt-link to="character">Character</nuxt-link>
           </li>
-          <li class="mx-1 p-3">
+          <li class="h-3 mx-1 p-2">
             <nuxt-link to="quests">Quests</nuxt-link>
           </li>
-          <li class="mx-1 p-3">
+          <li class="h-3 mx-1 p-2">
             <nuxt-link to="market">Market</nuxt-link>
           </li>
-          <li class="mx-1 p-3">
+          <li class="h-3 mx-1 p-2">
             <nuxt-link to="battle-board">Battle Board</nuxt-link>
           </li>
         </ul>
--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,7 @@ export default {
 
   purgeCSS: {
     // eslint-disable-next-line no-undef
-    enabled: 'false', // ({ isDev, isClient }) => !isDev && isClient or `false` when in dev/debug mode
+    enabled: ({ isDev, isClient }) => !isDev && isClient, // ({ isDev, isClient }) => !isDev && isClient or `false` when in dev/debug mode
     paths: [
       'components/**/*.vue',
       'layouts/**/*.vue',

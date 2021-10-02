9a8b79f8079f63cdbd979589249086d672611008
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-10-02 17:17:25 +0300

    feat: header modified

--- components/layouts/TheHeader.vue
+++ components/layouts/TheHeader.vue
@@ -2,19 +2,34 @@
   <div>
     <header>
       <nav class="d-flex h-10rem">
-        <h3 class="m-2 p-2"><nuxt-link to="/"> Mob Fighter</nuxt-link></h3>
+        <h1 class="my-1 mx-2 p-2 shadow-text">
+          <nuxt-link to="/"> Mob Fighter</nuxt-link>
+        </h1>
         <ul class="d-flex m-0">
-          <li class="h-3 mx-1 p-2">
-            <nuxt-link to="character">Character</nuxt-link>
+          <li class="m-2 p-1 shadow-text">
+            <ui-base-button link to="character" classes="outline shadow-box"
+              >Character</ui-base-button
+            >
           </li>
-          <li class="h-3 mx-1 p-2">
-            <nuxt-link to="quests">Quests</nuxt-link>
+          <li class="m-2 p-1 shadow-text">
+            <ui-base-button link to="quests" classes="outline shadow-box"
+              >Quests</ui-base-button
+            >
           </li>
-          <li class="h-3 mx-1 p-2">
-            <nuxt-link to="market">Market</nuxt-link>
+          <li class="m-2 p-1 shadow-text">
+            <ui-base-button link to="market" classes="outline shadow-box"
+              >Market</ui-base-button
+            >
           </li>
-          <li class="h-3 mx-1 p-2">
-            <nuxt-link to="battle-board">Battle Board</nuxt-link>
+          <li class="m-2 p-1 shadow-text">
+            <ui-base-button link to="battle-board" classes="outline shadow-box"
+              >Battle Board</ui-base-button
+            >
+          </li>
+          <li class="m-2 p-1 shadow-text ml-20">
+            <ui-base-button link to="login" classes="filled shadow-box"
+              >Login</ui-base-button
+            >
           </li>
         </ul>
       </nav>
@@ -25,8 +40,11 @@
 <style lang="scss" scoped>
 ul {
   list-style: none;
-  li {
-    border: 2px solid black;
-  }
+}
+header {
+  border: 3px solid #996900;
+}
+.shadow-text {
+  text-shadow: 0 0 2rem white, 1px 1px 2rem white, -1px -1px 2rem white;
 }
 </style>

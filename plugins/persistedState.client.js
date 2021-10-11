9f5ab23a23a520aefa99a9c40a8f4cf9cbdc13db
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-10-11 14:47:12 +0300

    fix: persistedState store changed to local

--- plugins/persistedState.client.js
+++ plugins/persistedState.client.js
@@ -1,14 +1,7 @@
 import createPersistedState from 'vuex-persistedstate'
-import Cookies from 'js-cookie'
 
 export default ({ store }) => {
   createPersistedState({
     key: 'character',
-    storage: {
-      getItem: (key) => Cookies.get(key),
-      setItem: (key, value) =>
-        Cookies.set(key, value, { expires: 60 * 60 * 24 * 365, secure: true }),
-      removeItem: (key) => Cookies.remove(key),
-    },
   })(store)
 }

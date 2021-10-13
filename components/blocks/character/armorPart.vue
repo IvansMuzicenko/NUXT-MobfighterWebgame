2de460e30fa7f411addc871f6abc8717fba9b811
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-10-13 15:45:19 +0300

    feat: character block splitted into components

---
+++ components/blocks/character/armorPart.vue
@@ -0,0 +1,42 @@
+<template>
+  <li>
+    <span v-if="part" :class="part.rarity">{{ label }}: </span>
+    <span v-if="part == null">{{ label }}: none</span>
+    <span v-else>
+      <span class="armor"> armor: {{ part.stats.ARMOR }}</span>
+      <span v-if="part.stats.STR != 0" class="str">
+        , strength: {{ part.stats.STR }}
+      </span>
+      <span v-if="part.stats.AGI != 0" class="agi">
+        , agility: {{ part.stats.AGI }}
+      </span>
+      <span v-if="part.stats.INT != 0" class="int">
+        , intelligence: {{ part.stats.INT }}
+      </span>
+      <ui-base-button class="outline--small" @click="unequipItem()">
+        Unequip
+      </ui-base-button>
+    </span>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    part: {
+      type: Object,
+      required: false,
+      default: null,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    unequipItem() {
+      this.$emit('unequipItem', this.part)
+    },
+  },
+}
+</script>
---
+++ components/blocks/character/weaponPart.vue
@@ -0,0 +1,38 @@
+<template>
+  <li>
+    <span v-if="part" :class="part.rarity">{{ label }}: </span>
+    <span v-if="part == null">{{ label }}: none</span>
+    <span v-else>
+      <span v-if="part.stats.attackPower != 0" class="str">
+        Attack Power: {{ part.stats.attackPower }}</span
+      >
+      <span v-if="part.stats.spellPower != 0" class="int">
+        Spell Power: {{ part.stats.spellPower }}</span
+      >
+      <ui-base-button class="outline--small" @click="unequipItem()"
+        >Unequip</ui-base-button
+      >
+    </span>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    part: {
+      type: Object,
+      required: false,
+      default: null,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    unequipItem() {
+      this.$emit('unequipItem', this.part)
+    },
+  },
+}
+</script>

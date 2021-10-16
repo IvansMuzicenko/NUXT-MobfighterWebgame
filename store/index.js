a8fed103e0c1399609fe7bf3f427596b4acb6fdc
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-10-16 09:58:08 +0300

    refactor: store divided on modules

--- store/index.js
+++ store/index.js
@@ -33,14 +33,20 @@ export const state = () => ({
         LHand: null,
         RHand: null,
         THand: null,
-        reserve: null,
+        reserve: {
+          type: 'weapon',
+          slot: 'reserve',
+          rarity: 'common',
+          cost: '0',
+          name: 'knife',
+          key: '0',
+          stats: {
+            attackPower: 1,
+          },
+        },
       },
     },
     items: [],
-    market: {
-      lastUpdate: null,
-      products: [],
-    },
   },
 })
 
@@ -91,15 +97,6 @@ export const mutations = {
     )
   },
 
-  BUY_ITEM(state, product) {
-    if (state.character.money >= product.cost) {
-      state.character.money -= product.cost
-      state.character.items.push(product)
-      state.character.market.products = state.character.market.products.filter(
-        (item) => item.key !== product.key
-      )
-    }
-  },
   EQUIP_ITEM(state, equipItem) {
     const equippedItem =
       state.character.equipment[equipItem.type.toLowerCase()][equipItem.slot]
@@ -153,124 +150,6 @@ export const mutations = {
       depStats.MP += 5
     }
   },
-  GENERATE_PRODUCTS(state) {
-    state.character.market.products = []
-    const armorSlots = ['head', 'shoulder', 'chest', 'arms', 'leggins', 'boots']
-    const weaponSlots = ['LHand', 'RHand', 'THand', 'reserve']
-    const market = state.character.market
-    const lvl = state.character.lvl
-    market.lastUpdate = Date.now()
-
-    for (let i = 0; i < 10; i++) {
-      let itemType = ''
-      let itemSlot = ''
-      let itemRarity = ''
-      let itemCost = null
-      let itemName = ''
-      let grip = ''
-      const itemKey = Date.now() + i
-      let attrPoints = null
-
-      let armorPoints = null
-      let strPoints = null
-      let agiPoints = null
-      let intPoints = null
-      let atckPowerPoints = null
-      let splPowerPoints = null
-
-      const rarityRand = Math.floor(Math.random() * 100)
-      if (rarityRand <= 70) {
-        itemRarity = 'common'
-        attrPoints = lvl
-      } else if (rarityRand > 70 && rarityRand <= 90) {
-        itemRarity = 'rare'
-        attrPoints = (lvl * 1.5).toFixed()
-      } else if (rarityRand > 90 && rarityRand <= 98) {
-        itemRarity = 'epic'
-        attrPoints = (lvl * 2).toFixed()
-      } else if (rarityRand > 98 && rarityRand <= 100) {
-        itemRarity = 'legendary'
-        attrPoints = (lvl * 3).toFixed()
-      }
-
-      if (Math.ceil(Math.random() * 10) <= 7) {
-        itemType = 'armor'
-        itemSlot = armorSlots[Math.floor(Math.random() * 6)]
-        itemName = itemSlot
-        for (let i = 0; i < attrPoints; i++) {
-          const randStat = Math.ceil(Math.random() * 4)
-          armorPoints = lvl
-          if (randStat === 1) {
-            armorPoints += 1
-          } else if (randStat === 2) {
-            strPoints += 1
-          } else if (randStat === 3) {
-            agiPoints += 1
-          } else {
-            intPoints += 1
-          }
-        }
-      } else {
-        itemType = 'weapon'
-        itemSlot = weaponSlots[Math.floor(Math.random() * 4)]
-        if (itemSlot === 'THand') {
-          grip = 'Two-handed'
-          if (Math.random() <= 0.5) {
-            itemName = 'sword'
-            atckPowerPoints = attrPoints * 2.5
-          } else {
-            itemName = 'staff'
-            splPowerPoints = attrPoints * 2.5
-          }
-        } else if (itemSlot === 'LHand') {
-          grip = 'Left-handed'
-          if (Math.random() <= 0.5) {
-            itemName = 'dagger'
-            atckPowerPoints = attrPoints
-          } else {
-            itemName = 'wand'
-            splPowerPoints = attrPoints
-          }
-        } else if (itemSlot === 'RHand') {
-          grip = 'Right-handed'
-          if (Math.random() <= 0.5) {
-            itemName = 'dagger'
-            atckPowerPoints = attrPoints
-          } else {
-            itemName = 'wand'
-            splPowerPoints = attrPoints
-          }
-        } else if (itemSlot === 'reserve') {
-          itemName = 'knife'
-          grip = 'Reserve'
-          atckPowerPoints = attrPoints / 2
-        }
-      }
-
-      const itemStats = {
-        ARMOR: Math.ceil(armorPoints),
-        STR: Math.ceil(strPoints),
-        AGI: Math.ceil(agiPoints),
-        INT: Math.ceil(intPoints),
-        attackPower: Math.ceil(atckPowerPoints),
-        spellPower: Math.ceil(splPowerPoints),
-      }
-      for (const stat in itemStats) {
-        itemCost += itemStats[stat]
-      }
-
-      const newItem = {
-        type: itemType,
-        slot: itemSlot,
-        rarity: itemRarity,
-        cost: itemCost,
-        name: grip + ' ' + itemName,
-        key: itemKey,
-        stats: itemStats,
-      }
-      market.products.push(newItem)
-    }
-  },
 }
 
 export const actions = {
@@ -282,10 +161,7 @@ export const actions = {
     commit('SELL_ITEM', soldItem)
     dispatch('saveData')
   },
-  buyItem({ commit, dispatch }, product) {
-    commit('BUY_ITEM', product)
-    dispatch('saveData')
-  },
+
   equipItem({ commit, dispatch }, equipItem) {
     commit('EQUIP_ITEM', equipItem)
     dispatch('saveData')
@@ -300,9 +176,7 @@ export const actions = {
       dispatch('saveData')
     }, 5000)
   },
-  generateProducts({ commit }) {
-    commit('GENERATE_PRODUCTS')
-  },
+
   saveData({ commit }) {
     commit('CALC_STATS')
   },

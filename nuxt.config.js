a390f28b57ff4d68a4e5f9e53505fc13b546b415
Aleksejs Muzicenko <a.muzicenko@dynapay.co.uk>
2021-10-22 14:09:32 +0300

    feat: added tailwind with my realisation

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,7 @@ export default {
     '@nuxtjs/stylelint-module',
     // https://www.npmjs.com/package/@nuxtjs/style-resources
     '@nuxtjs/style-resources',
+    '@nuxtjs/tailwindcss',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
